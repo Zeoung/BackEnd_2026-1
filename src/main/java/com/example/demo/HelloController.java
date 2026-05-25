a926c8822e00d128374387a58db81040510016b9
Hyoungmin Kim <hm01053882264@gmail.com>
2026-05-25 23:01:05 +0900

    feat: GET /introduce

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,12 +10,15 @@ public class HelloController {
 
     @ResponseBody
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
-    }
-
-    @GetMapping("/hello2")
+    @@ -17,4 +18,10 @@
     public String hello2() {
         return "hello";
     }
+
+    @ResponseBody
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
+    }
 }
+
---
+++ src/main/java/com/example/demo/JsonContronller.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping("/hello")
+    @@ -24,4 +27,15 @@
+    public String introduce(@RequestParam String name) {
+        return "안녕하세요 제 이름은 " + name + "입니다!";
+    }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("age", 25);
+        result.put("name", "박준용");
+
+        return result;
+    }
+}
+
